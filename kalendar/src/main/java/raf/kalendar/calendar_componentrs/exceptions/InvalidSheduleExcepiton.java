a201f88149cdacd1f55fdd8e29291eed556c1562
Tamara <Tamara@192.168.0.10>
2018-04-11 23:49:52 +0200

    Added docblock for exception

--- kalendar/src/main/java/raf/kalendar/calendar_componentrs/exceptions/InvalidSheduleExcepiton.java
+++ kalendar/src/main/java/raf/kalendar/calendar_componentrs/exceptions/InvalidSheduleExcepiton.java
@@ -2,6 +2,11 @@ package raf.kalendar.calendar_componentrs.exceptions;
 
 import raf.kalendar.calendar_componentrs.model.Event;
 
+/**
+ * 
+ * @author Tamara
+ *
+ */
 public class InvalidSheduleExcepiton extends Exception {
 
 	public InvalidSheduleExcepiton(Event event1, Event event2) {
